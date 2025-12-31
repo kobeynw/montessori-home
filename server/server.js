f06acc0715f3aeeca293555a3cac79ecd2c43eaa
Kobey Workman <kobeyworkman1@gmail.com>
2025-12-30 22:54:49 -0700

    Update server to fix typo

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.unsubscribe(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
