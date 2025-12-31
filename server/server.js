a0572f8837f9e0b50bbcfe731171b58363c12bdb
Kobey Workman <kobeyworkman1@gmail.com>
2025-12-30 23:30:50 -0700

    Update backend port to use 5001

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
